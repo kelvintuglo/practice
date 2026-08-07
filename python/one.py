3016608a15a679d145217c0f93529114fa856476
kelvintuglo <kelvinkusha@gmail.com>
2026-08-07 21:33:55 +0000

    python practice for first sem

---
+++ python/one.py
@@ -0,0 +1,58 @@
+print("Hello World!")
+name = "Kelvin"
+print("Wagwan," + name)
+
+# naming coneventions practice
+i = "boy"
+print(i)
+
+I = "girl"
+print(I)
+
+f = "toy"
+print(f)
+
+hand = "five"
+print(hand)
+
+_leg = "ten"
+print(_leg)
+
+teeth = "32"
+print(teeth)
+
+firstAge = 26
+print(firstAge)
+
+first_number = 32
+print(first_number)
+
+PI = 3.147
+print(PI)
+
+print(type(50))
+print(type(50.0))
+
+
+# score1 = float(input("Enter your score: "))
+# print(score1)
+
+# score2 = int(input("enter your second score: "))
+# print(score2)
+
+mix = ["apple", "pawpaw", 25]
+print(mix[0])
+print(mix[1])
+print(mix[2])
+
+print(type(mix[0]))
+print(type(mix[1]))
+print(type(mix[2]))
+
+print(mix)
+
+
+mix[0] = "banana"
+mix[1] = "dates"
+
+print(mix)
