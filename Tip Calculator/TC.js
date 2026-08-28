ba42cad1bee6ad48e6ba96756f4199dbb171cd3d
kelvintuglo <kelvinkusha@gmail.com>
2026-08-28 15:41:09 +0000

    Tip calculator done
    This mini app takes a bill amount and a tip percentage and outputs the tip amount and the total

--- Tip Calculator/TC.js
+++ Tip Calculator/TC.js
@@ -2,11 +2,15 @@ let billAmount;
 let tipPercent;
 
 document.getElementById("calc").onclick = function(){
-  if (document.getElementById("bill-amt").value === "" || document.getElementById("tip-per").value === "") {
-    document.getElementById("text").textContent = `Error: enter the bill amount and the tip percent.`;
+  if (document.getElementById("bill-amt").value === "" && document.getElementById("tip-per").value === "") {
+    document.getElementById("text").textContent = `Error: enter the Bill Amount and the Tip Percent.`;
+    document.getElementById("text").style.color = "brown";
+  } else if(document.getElementById("bill-amt").value === ""){
+      document.getElementById("text").textContent = `Error: please enter the bill amount.`;
+      document.getElementById("text").style.color = "black";
+  } else if (document.getElementById("tip-per").value === "") {
+    document.getElementById("text").textContent = `Error: please enter the tip percentage.`;
     document.getElementById("text").style.color = "red";
-  } else if(dfj){
-      //figure this shit out man
   } else {
     billAmount = document.getElementById("bill-amt").value;
     billAmount = Number(billAmount);
@@ -14,17 +18,16 @@ document.getElementById("calc").onclick = function(){
     tipPercent = Number(tipPercent);
 
     if (isNaN(billAmount) || isNaN(tipPercent)) {
-      document.getElementById("text").textContent = `Please enter valid numbers.`;
-      document.getElementById("text").style.color = "black";
+      document.getElementById("text").textContent = `Error: please enter valid numbers.`;
+      document.getElementById("text").style.color = "firebrick";
     } else {
       let result = billAmount * tipPercent;
-      document.getElementById("text").textContent = `Your tip is GHS ${result}`;
+      document.getElementById("text").textContent = `Your tip is GHS ${result}.`;
       document.getElementById("text").style.color = "green";
+
+      let total = billAmount + result;
+      document.getElementById("total").textContent = `Your total is GHS ${total}.`;
+      document.getElementById("total").style.color = `blue`;
     }
   }
-  // let total = billAmount + result;
-  // document.getElementById("text").textContent = `Your total is GHS ${total}`;
-  // document.getElementById("text").style.color = `blue`;
 }
-
-//1.09.02. timestamp
