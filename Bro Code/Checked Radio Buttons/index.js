81c8434f77c3f5aa7e7c20acbb7dfb36191e272e
kelvintuglo <kelvinkusha@gmail.com>
2026-08-30 19:12:29 +0000

    Checked radio buttons

---
+++ Bro Code/Checked Radio Buttons/index.js
@@ -0,0 +1,30 @@
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const mastercardBtn = document.getElementById("mastercardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+  if (myCheckbox.checked) {
+    subResult.textContent = "You are subscribed";
+  }
+  else {
+    subResult.textContent = "You are not subscribed";
+    paymentResult.textContent = "";
+  }
+
+  if (visaBtn.checked) {
+    paymentResult.textContent = "You are paying with Visa";
+  }
+  else if (mastercardBtn.checked){
+    paymentResult.textContent = "You are paying with Mastercard";
+  }
+  else if (paypalBtn.checked) {
+    paymentResult.textContent = "You are paying with Paypal";
+  } 
+  else {
+    paymentResult.textContent = "You must select a payment type";
+  }
+}
